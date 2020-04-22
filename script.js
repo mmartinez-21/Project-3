c8911dfc8fceac81d54b9511b6bc3850801e4174
mmartinez-21 <60053322+mmartinez-21@users.noreply.github.com>
2020-04-21 22:17:13 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function init(){
+  var object1 = document.getElementById('myMap');
+  var startingLoc = new google.maps.LatLng(41.8433677,-87.6948927);
+  var mapOptions = {
+    center: startingLoc,
+    zoom: 8,
+    mapTypeId: google.maps.MapTypeId.SATELLITE,
+    mapTypeControlOptions: {
+      position: google.maps.ControlPosition.BOTTOM_CENTER
+    }
+  };
+}
